57813bd2bdd6a5a924cdd6abd5bc09920d2c85c0
Adam J. Leuer <aleuer2@illinois.edu>
2019-01-04 16:17:34 -0600

    Created project

---
+++ RGBEdit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AuraSDKDotNet;
+
+namespace RGBEdit
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+           var sdk = new AuraSDK();
+           
+           AuraDevice board = sdk.Motherboards[0];
+           
+        }
+    }
+}
