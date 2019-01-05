43722c48c77879f3012cf23adb683365204b0d5e
Adam J. Leuer <aleuer2@illinois.edu>
2019-01-05 04:51:48 -0600

    Created RGBEditNative project

---
+++ RGBEditNative/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <thread>
+
+int main(int numberOfArguments, char * arguments[])
+{
+	std::cout << "Hi" << std::endl;
+	std::system("pause");
+	// std::this_thread::sleep_for(std::chrono::seconds(5));
+}
