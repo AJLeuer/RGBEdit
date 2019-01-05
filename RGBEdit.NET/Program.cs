55d7554fe96437a4690d7f0d2f3124b918e0c5af
Adam J. Leuer <aleuer2@illinois.edu>
2019-01-05 04:06:57 -0600

    Created RGBEdit.NET project

---
+++ RGBEdit.NET/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using AuraSDKDotNet;
+
+namespace RGBEdit.NET
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            String currentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            String auraSDKPath = $"{currentDirectory}\\Libraries\\AuraSDK\\sdk\\AURA_SDK.dll";
+            
+            var sdk = new AuraSDK(path: auraSDKPath);
+           
+            AuraDevice board = sdk.Motherboards[0];
+        }
+    }
+}
--- RGBEdit/Program.cs
+++ RGBEdit/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.IO;
 using AuraSDKDotNet;
 
 namespace RGBEdit
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-           var sdk = new AuraSDK();
+            String currentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            String auraSDKPath = $"{currentDirectory}\\Libraries\\AURA_SDK.dll";
+            
+            var sdk = new AuraSDK(path: auraSDKPath);
            
-           AuraDevice board = sdk.Motherboards[0];
+            AuraDevice board = sdk.Motherboards[0];
            
         }
     }
